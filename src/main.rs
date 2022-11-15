dcd77a9b33137fe42d2f4c3fa388aba37f04cfda
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-11-14 20:02:16 -0500

    feat: add file IO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs;
+
+mod parser;
+use crate::parser::parse;
+
 fn main() {
-    println!("Hello, world!");
+    for file_name in env::args().skip(1) {
+        let Ok(string) = fs::read_to_string(&file_name) else {
+            eprintln!("Can not read file {}", file_name);
+            continue;
+        };
+
+        println!("Reading {}", file_name);
+
+        match parse(&string) {
+            Ok(()) => (),
+            Err(error) => eprintln!("{}", error),
+        }
+    }
 }
---
+++ src/parser.rs
@@ -0,0 +1,23 @@
+use std::fmt;
+
+pub struct ParsingError {
+    line: usize,
+    column: usize,
+    expected: String,
+    found: String,
+}
+
+impl fmt::Display for ParsingError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            formatter,
+            "Error on line {} column {} expected {} but found {}.",
+            self.line, self.column, self.expected, self.found
+        )
+    }
+}
+
+/// Parses a iterator of characters as css
+pub fn parse(input: &str) -> Result<(), ParsingError> {
+    todo!()
+}
