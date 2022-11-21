a5daaf728f790bcbd4cb17a33ed491079922d74b
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-11-21 15:31:43 -0500

    feat: implement `FromStr` for `DistanceUnit`

--- src/parser/length.rs
+++ src/parser/length.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Debug, PartialEq)]
 pub enum Distance {
     Zero(),
@@ -18,4 +20,28 @@ pub enum DistanceUnit {
     RootInlineSize,  // vi
     ViewportMinimum, // vmin
     ViewportMaximum, // vmax
+}
+
+impl FromStr for DistanceUnit {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use DistanceUnit::*;
+
+        match s {
+            "px" => Ok(Pixels),
+            "cm" => Ok(Centimeters),
+            "in" => Ok(Inches),
+            "pt" => Ok(Points),
+            "em" => Ok(FontSize),
+            "rem" => Ok(RootFontSize),
+            "vh" => Ok(ViewportHeight),
+            "vw" => Ok(ViewportWidth),
+            "vb" => Ok(RootBlockSize),
+            "vi" => Ok(RootInlineSize),
+            "vmin" => Ok(ViewportMinimum),
+            "vmax" => Ok(ViewportMaximum),
+            _ => Err(()),
+        }
+    }
 }
