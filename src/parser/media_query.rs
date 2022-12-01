3a116123546d251fbe051ebbf7dcfd45930b392d
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-11-30 21:18:53 -0500

    feat: add MediaQuery type

--- src/parser/media_query.rs
+++ src/parser/media_query.rs
@@ -3,3 +3,14 @@ use crate::tokenizer::*;
 
 mod media_feature;
 mod media_type;
+
+use media_feature::*;
+use media_type::*;
+
+pub enum MediaQuery {
+    MediaType(MediaType),
+    MediaFeature(MediaFeature),
+    Not(Box<MediaQuery>),
+    And(Box<MediaQuery>, Box<MediaQuery>),
+    Or(Box<MediaQuery>, Box<MediaQuery>),
+}
