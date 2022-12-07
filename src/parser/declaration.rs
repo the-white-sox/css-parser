70fc5d7d3776c66948b93b8cea40caeecedaefc5
njumaah <njumaah@emich.edu>
2022-12-07 14:06:49 -0500

    feat: :poop: need to pull from main

--- src/parser/declaration.rs
+++ src/parser/declaration.rs
@@ -8,3 +8,22 @@
 // <sides-color-property> ::= "border-color"
 // <length-property> ::= "font-size" | "height" | "width"
 // <side-lengths-property> ::= "margin" | "padding" | "border-width" | "border-radius"
+
+use super::*;
+use crate::tokenizer::*;
+
+#[derive(Debug, PartialEq)]
+
+pub enum declaration {
+    BackgroundColor(Color),
+    BorderColor(Color),
+    Opacity(f64),
+    FontFamily(FontFamily),
+    FontSize(Length),
+    Height(Length),
+    Width(Length),
+    Margin(Length),
+    Padding(Length),
+    BorderWidth(Length),
+    BorderRadius(Length),
+}
