ddd8bb8e3d39b8ff879cc5604849db4761ca7855
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-12-12 12:22:26 -0500

    feat: parse RuleSet

--- src/parser/rule/ruleset.rs
+++ src/parser/rule/ruleset.rs
@@ -1,10 +1,42 @@
+use super::basic_selector::BasicSelector;
+use super::declaration::Declaration;
+
 use super::*;
 
 #[derive(Debug, PartialEq)]
-pub struct Ruleset {}
+pub struct Ruleset {
+    pub selectors: Vec<BasicSelector>,
+    pub declarations: Vec<Declaration>,
+}
 
 impl Parsable for Ruleset {
-    fn parse<I: Iterator<Item = char>>(_parser: &mut Parser<I>) -> Result<Self, ParsingError> {
-        todo!()
+    fn parse<I: Iterator<Item = char>>(parser: &mut Parser<I>) -> Result<Self, ParsingError> {
+        let selectors = parser.parse()?;
+        parser.expect(Token::Delimiter('{'))?;
+        let declarations = parser.parse()?;
+        parser.expect(Token::Delimiter('}'))?;
+        Ok(Ruleset {
+            selectors,
+            declarations,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::basic_selector::BasicSelector;
+    use super::declaration::Declaration;
+    use super::*;
+
+    #[test]
+    fn empty_ruleset() {
+        let mut parser = Parser::new("* {}".chars());
+        assert_eq!(
+            Ok(Ruleset {
+                selectors: vec![BasicSelector::Universal()],
+                declarations: vec![]
+            }),
+            parser.parse()
+        );
     }
 }
