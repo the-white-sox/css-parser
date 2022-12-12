e03307fa2cf965d39a4aff9093e5ef2d2c551a97
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-12-12 13:02:29 -0500

    test: add tests for Ruleset

--- src/parser/rule/ruleset.rs
+++ src/parser/rule/ruleset.rs
@@ -27,6 +27,7 @@ mod tests {
     use super::basic_selector::BasicSelector;
     use super::declaration::Declaration;
     use super::*;
+    use crate::parser::color::Color;
 
     #[test]
     fn empty_ruleset() {
@@ -38,5 +39,56 @@ mod tests {
             }),
             parser.parse()
         );
+        assert_eq!(None, parser.tokens.next());
+    }
+
+    #[test]
+    fn multiple_declarations() {
+        let mut parser = Parser::new("* {background-color: blue; opacity: 0.7}".chars());
+        assert_eq!(
+            Ok(Ruleset {
+                selectors: vec![BasicSelector::Universal()],
+                declarations: vec![
+                    Declaration::BackgroundColor(Color::Blue),
+                    Declaration::Opacity(0.7)
+                ]
+            }),
+            parser.parse()
+        );
+        assert_eq!(None, parser.tokens.next());
+    }
+
+    #[test]
+    fn multiple_selectors() {
+        let mut parser = Parser::new("div, #fab {background-color: blue}".chars());
+        assert_eq!(
+            Ok(Ruleset {
+                selectors: vec![
+                    BasicSelector::Element("div".to_string()),
+                    BasicSelector::Id("fab".to_string())
+                ],
+                declarations: vec![Declaration::BackgroundColor(Color::Blue)]
+            }),
+            parser.parse()
+        );
+        assert_eq!(None, parser.tokens.next());
+    }
+
+    #[test]
+    fn no_selector() {
+        let mut parser = Parser::new("{background-color: blue}".chars());
+        assert!(parser.parse::<Ruleset>().is_err());
+    }
+
+    #[test]
+    fn no_opening_brace() {
+        let mut parser = Parser::new("* background-color: blue}".chars());
+        assert!(parser.parse::<Ruleset>().is_err());
+    }
+
+    #[test]
+    fn no_closing_brace() {
+        let mut parser = Parser::new("* {background-color: blue".chars());
+        assert!(parser.parse::<Ruleset>().is_err());
     }
 }
